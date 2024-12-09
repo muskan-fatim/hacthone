5ae1edae92c346e82e9b6a3798cb63215861dfa9
Muskan Fatima <160476952+muskan-fatim@users.noreply.github.com>
2024-12-09 11:36:40 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images:{
     domains:['s3-alpha-sig.figma.com']
-  }
+  },
    eslint:{
     ignoreDuringBuilds:true
   }
